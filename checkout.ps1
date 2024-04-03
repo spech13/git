468f312c86783c00e456f1460cdc0d6adad4da52
spech13 <es-popov2@yandex.ru>
2024-04-03 10:01:33 +0300

    Move git checkout command to checkout.ps1 file

---
+++ checkout.ps1
@@ -0,0 +1,9 @@
+# git checkout <commit_id> - switch to specified commit with id "commit_id"
+# git checkout <branch_name> - switch to specified branch with name "branch_name"
+# git checkout -b <branch_name> - create and switch to specified branch with name "branch_name"
+# git checkout -b <new_branch_name> <current_branch_name> - create and switch to branch with name "new_branch_name" and same state as "current_branch_name"
+
+git checkout 484aa0d
+git checkout -b KUB-1212
+git checkout KUB-1212
+git checkout -b KUB-1212 KUB-1213

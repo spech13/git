481463137e59176ed7003c6aa3e03c084607f1b8
spech13 <es-popov2@yandex.ru>
2024-05-06 22:04:33 +0300

    Add remote command description

---
+++ commit.ps1
@@ -0,0 +1,5 @@
+# git commit -m <comment> - approve all changes with comment "comment"
+git commit -m "my comment"
+
+# git commit --amend - change commit message
+git commit --amend  
---
+++ remote.ps1
@@ -0,0 +1,6 @@
+# git remote add <name> <url> - add remote connection with github repository.
+# "name" - name of connection, "url" - url of repository
+git remote add git@github.com:spech13/git.git origin 
+
+# git remote get-url <name> - get url of repository by his name
+git remote get-url origin

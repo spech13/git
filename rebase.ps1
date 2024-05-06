c5c836329a6adb4e854c8e1f07bac965b5cea6aa
spech13 <es-popov2@yandex.ru>
2024-05-06 22:08:26 +0300

    Add rebase command description

---
+++ rebase.ps1
@@ -0,0 +1,5 @@
+# git rebase -i HEAD~<commits_count> - interactive rebase with possible rename, squash and so on of commits on current branch
+git rebase -i HEAD~3
+
+# git rebase -i <baranch_name> - interactive rebase with possible rename, squash and so on of commits to specified branch with name "branch_name"
+git rebase -i BRN-dev

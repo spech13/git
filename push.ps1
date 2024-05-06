3eac5d58310dc3b44333105f6e8359578b3bd9d9
spech13 <es-popov2@yandex.ru>
2024-05-06 22:21:03 +0300

    Add push command description

---
+++ push.ps1
@@ -0,0 +1,5 @@
+# git push -u origin <branch_name> - push changes to remote branch with name "branch_name"
+git push -u origin main
+
+# git push origin --delete <branch_name> - delete remote branch named "branch_name"
+git push oring --delete BRN-1212

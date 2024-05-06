d97d50ad6cf7d7e97fe7d5205da8f5953d983401
spech13 <es-popov2@yandex.ru>
2024-05-06 21:52:49 +0300

    Add branch command description

---
+++ branch.ps1
@@ -0,0 +1,12 @@
+# git branch <branch_name> - create new branch with name "branch_name"
+git branch BRN-1212
+
+# git branch -a - output all branches
+git branch -a
+
+# git branch -m <current_branch_name> <new_branch_name> - rename branch
+# from "current_branch_name" to "new_branch_name"
+git branch -m BRN-1212 BRN-1313
+
+# git branch --show-current - show current branch name
+git branch --show-current
